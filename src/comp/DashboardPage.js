1d892c22beb855b72ff62708a61a1c8bc49e8cbe
Kyle Corbin <kylecorbin26@gmail.com>
2020-01-22 22:44:24 -0500

    Got the dashbaord looking a bit better, restyled the locations and items, added positioning to their labels

--- src/comp/DashboardPage.js
+++ src/comp/DashboardPage.js
@@ -27,10 +27,10 @@ const Dashboard =()=>{
         <div className="Dashboard">
             <DashboardUtils/>
             <section className="DashboardPage">
-                <h1>Welcome:{state.user.username}</h1>
+                <h1 className="DashTitle">Welcome:{state.user.username}</h1>
                 <div className="Dashboard-flex">
                     <section className="Dash-item Locations">
-                        <h1>Your Locations:</h1>
+                        <h1 className="subDashTitle">Your Locations:</h1>
                         {state.user.locations.map((loc,index)=>(
                             <div className={`location location-${index}`} >
                                 <h3>{loc.name}</h3>
@@ -38,7 +38,7 @@ const Dashboard =()=>{
                         ))}
                     </section>
                     <section className="Dash-item Items">
-                        <h1>Your Items:</h1>
+                        <h1 className="subDashTitle">Your Items:</h1>
                         {state.user.items.map((item,index)=>(
                             <div className={`item item-${index}`} key={index} >
                                 <h3>{item.name}</h3>
