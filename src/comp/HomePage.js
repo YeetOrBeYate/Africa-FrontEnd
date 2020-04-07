8924e61483c95dd6248f23c510d57a38b28665f4
Kyle Corbin <kylecorbin26@gmail.com>
2020-04-07 14:03:23 -0400

    started the homepage, animations and styling still need to be aded

--- src/comp/HomePage.js
+++ src/comp/HomePage.js
@@ -6,6 +6,14 @@ const Home = ()=>{
     return(
         <div className = "Homepage">
             <h1>Homepage</h1>
+            <div>
+                <section>
+                    <p>Sauti Africa gives small business owners, particularly women, the opportunity to improve their businesses by providing a market information platform.More information on what they do can be found <a href='https://sautiafrica.org/' target='_blank'>here</a></p>
+                </section>
+                <section>
+                    <p>This app acts as a simulation for what Sauto Africa has implemented. A user can see other shop owners locations and item prices, and add an item or location listing to their profile.</p>
+                </section>
+            </div>
         </div>
     );
 }
