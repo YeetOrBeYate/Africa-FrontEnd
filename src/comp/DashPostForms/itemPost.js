6aaa88d5533dd3dc9255267d6cba1acc5e691e18
Kyle Corbin <kylecorbin26@gmail.com>
2020-04-15 14:12:13 -0400

    Got the add item form validation working

--- src/comp/DashPostForms/itemPost.js
+++ src/comp/DashPostForms/itemPost.js
@@ -17,59 +17,59 @@ const ItemYeet = ()=>{
         price:'',
         user_id: User.user.id,
         category_id:null,
-        location_id:null
+        location_id:null,
+        postFail:false
     })
 
     const changePostItem = (e)=>{
-        setPostItem({...postItem, [e.target.name]:e.target.value})
+        setPostItem({...postItem, [e.target.name]:e.target.value, postFail:false})
     }
 
     const sendItem=(e)=>{
         e.preventDefault()
-        
-        const newPost= {
-            name: postItem.name,
-            description: postItem.description,
-            price:Number(postItem.price),
-            user_id:postItem.user_id,
-            category_id:Number(postItem.category_id),
-            location_id:Number(postItem.location_id)
+
+        if(!postItem.name || !Number(postItem.price) ||!postItem.category_id || !postItem.location_id){
+            setPostItem({...postItem, postFail:true})
+        }else{
+            const newPost= {
+                name: postItem.name,
+                description: postItem.description,
+                price:Number(postItem.price),
+                user_id:postItem.user_id,
+                category_id:Number(postItem.category_id),
+                location_id:Number(postItem.location_id)
+            }
+            dispatch(AddItem(newPost))
         }
-
         
-        
-        console.log("what i'll send",newPost)
-
-
-        dispatch(AddItem(newPost))
-        // replace with a item action so I can get this out of here
     }
 
     return(
             <form className="itemPostForm">
                 <div className="formDiv">
-                    <input type="text" name="name" onChange={changePostItem} value={postItem.name} placeholder="Item Name"/>
+                    <input type="text" name="name" onChange={changePostItem} value={postItem.name} required placeholder="Item Name"/>
                 </div>
                 <div className="formDiv">
-                    <input type='text' name="price" onChange={changePostItem} value={postItem.price} placeholder="Item Price"/>
+                    <input type='text' name="price" onChange={changePostItem} value={postItem.price} required placeholder="Item Price"/>
                 </div>
                 <div className="formDiv">
                     <textarea type="text" name="description" onChange={changePostItem} value={postItem.description} placeholder="Item Description" cols="31"/>
                 </div>
                 <div className="formDiv">
-                    <select onChange={changePostItem} name="category_id">
+                    <select onChange={changePostItem} required name="category_id">
                         <option>Pick a category</option>
                         {Category.categories.map((cat)=>(
                             <option value={cat.id}>{cat.name}</option>
                         ))}
                     </select> 
-                    <select onChange={changePostItem} name='location_id'>
-                        <option>Pick a location</option>
+                    <select onChange={changePostItem} required name='location_id'>
+                        <option value=''>Pick a location</option>
                         {User.user.locations.map((loc)=>(
                             <option value={loc.id}>{loc.name}</option>
                             ))}
                     </select>
                 </div>
+                {postItem.postFail? <b>Check form</b>: <></>}
                 <button id="formSubmit" onClick={(e)=>sendItem(e)}>Add Item</button>
             </form>
     );
