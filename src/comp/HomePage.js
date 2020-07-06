af0d5ff7d511311d6de977101e3d900465d38a53
Kyle Corbin <kylecorbin26@gmail.com>
2020-07-06 18:27:47 -0400

    Fixed typo on the homepage

--- src/comp/HomePage.js
+++ src/comp/HomePage.js
@@ -13,7 +13,7 @@ const Home = ()=>{
                     <p>Sauti Africa gives small business owners, particularly women, the opportunity to improve their businesses by providing a market information platform.More information on what they do can be found <a href='https://sautiafrica.org/' target='_blank' rel="noopener noreferrer">here</a></p>
                 </section>
                 <section>
-                    <p>This app acts as a simulation for what Sauto Africa has implemented. A user can see other shop owners locations and item prices, and add an item or location listing to their profile.</p>
+                    <p>This app acts as a simulation for what Sauti Africa has implemented. A user can see other shop owners locations and item prices, and add an item or location listing to their profile.</p>
                     <p>Africa icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
                 </section>
             </div>
